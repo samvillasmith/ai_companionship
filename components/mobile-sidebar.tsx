065757a8bec40d925e2d404c1d0fc35bed7afe04
samvillasmith <samuel.villa.smith@hotmail.com>
2025-01-09 22:08:32 -0600

    finished sidebar

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,26 @@
+import { Menu } from "lucide-react";
+
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet";
+
+import { Sidebar } from "@/components/sidebar";
+  
+
+export const MobileSidebar = () => {
+    return(
+        <Sheet>
+            <SheetTrigger className="md:hidden pr-4">
+                <Menu />
+            </SheetTrigger>
+            <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
+                <Sidebar />
+            </SheetContent>
+        </Sheet>
+    )
+}
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,7 +8,7 @@ import { UserButton } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button"; 
 import { ModeToggle } from "@/components/mode-toggle";
-
+import { MobileSidebar } from "@/components/mobile-sidebar";
 
 const font = Poppins({
     weight: "600",
@@ -18,8 +18,8 @@ const font = Poppins({
 export const Navbar = () => {
     return(
         <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-primary-10 bg-secondary h-16">
+            <MobileSidebar />
             <div className="flex items-center">
-                <Menu className="block md:hidden"/>
                 <Link href="/">
                     <h1 className={cn(
                         "hidden md:block text-xl md:text-3xl font-bold text-primary",
